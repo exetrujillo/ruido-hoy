d3ddb9af130b360f515f42822a518277aab1e61e
Exequiel Trujillo <exequiel.trujillo@ug.uchile.cl>
2026-04-05 23:43:48 -0400

    Corregida conexión DB x3

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 const sequelize = require('./config/database');
-const { User, Category, News, Comment } = require('./models');
+const { User, Category, News, Comment, DailyStat } = require('./models');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,25 +11,29 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
-// Routes
+let isSynced = false;
+app.use(async (req, res, next) => {
+    if (!isSynced && process.env.NODE_ENV !== 'test') {
+        try {
+            await sequelize.sync({ alter: true });
+            console.log('Database Synced (Lazy)');
+            isSynced = true;
+        } catch (err) {
+            console.error('Error syncing database on request:', err);
+        }
+    }
+    next();
+});
+
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/news', require('./routes/news.routes'));
 app.use('/api/categories', require('./routes/category.routes'));
 app.use('/api/comments', require('./routes/comment.routes'));
 
-sequelize.sync({ alter: true })
-    .then(() => {
-        console.log('Database synced');
-        if (!process.env.VERCEL) {
-            app.listen(PORT, () => {
-                console.log(`Servidor local corriendo en el puerto ${PORT}`);
-            });
-        }
-    })
-    .catch(err => {
-        console.error('Error al sincronizar la base de datos:', err);
+if (!process.env.VERCEL) {
+    app.listen(PORT, () => {
+        console.log(`Server local running on port ${PORT}`);
     });
+}
 
 module.exports = app;
-
-
