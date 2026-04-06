a16a81f60f0fb3e2f46d308820e73d6787cc2ab7
Exequiel Trujillo <exequiel.trujillo@ug.uchile.cl>
2026-04-05 23:37:31 -0400

    Corregida conexión DB

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,10 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-    process.env.POSTGRES_DATABASE || process.env.PGDATABASE,
-    process.env.POSTGRES_USER || process.env.PGUSER,
-    process.env.POSTGRES_PASSWORD || process.env.PGPASSWORD,
-    {
-        host: process.env.POSTGRES_HOST || process.env.PGHOST,
+const connString = process.env.DATABASE_URL || process.env.POSTGRES_URL;
+
+const sequelize = connString
+    ? new Sequelize(connString, {
         dialect: 'postgres',
         dialectOptions: {
             ssl: {
@@ -15,7 +13,22 @@ const sequelize = new Sequelize(
             }
         },
         logging: false
-    }
-);
+    })
+    : new Sequelize(
+        process.env.POSTGRES_DATABASE || process.env.PGDATABASE,
+        process.env.POSTGRES_USER || process.env.PGUSER,
+        process.env.POSTGRES_PASSWORD || process.env.PGPASSWORD,
+        {
+            host: process.env.POSTGRES_HOST || process.env.PGHOST,
+            dialect: 'postgres',
+            dialectOptions: {
+                ssl: {
+                    require: true,
+                    rejectUnauthorized: false
+                }
+            },
+            logging: false
+        }
+    );
 
 module.exports = sequelize;
